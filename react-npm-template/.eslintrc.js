1c55dd3617bb0161cec913b93f6fba768e442bf6
JunYang <yangjun@raina.tech>
2023-07-04 16:27:25 +0800

    feat: add eslint prettier recommand extends

--- react-npm-template/.eslintrc.js
+++ react-npm-template/.eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
   plugins: ['@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     'no-underscore-dangle': 'off',
   },
